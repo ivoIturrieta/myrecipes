cb862e61ec0a5eb38467195b0be04d477222c406
Ivo Iturrieta <iaiturri@uc.cl>
2017-09-18 23:56:09 +0000

    Refactor and clean out layout

--- db/migrate/20170918032951_create_recipes.rb
+++ db/migrate/20170918032951_create_recipes.rb
@@ -3,7 +3,7 @@ class CreateRecipes < ActiveRecord::Migration[5.0]
     create_table :recipes do |t|
       t.string :name
       t.string :email
-      t.timestampst.string :name
+      t.timestamps.string :name
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170918125124) do
+ActiveRecord::Schema.define(version: 20170918231758) do
 
   create_table "chefs", force: :cascade do |t|
     t.string   "chefname"
