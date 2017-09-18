35980a09f01e90b01c90804ef07e5df87a504a15
Ivo Iturrieta <iaiturri@uc.cl>
2017-09-18 12:48:52 +0000

    add chef and recipe model validations with test

---
+++ test/models/recipe_test.rb
@@ -0,0 +1,31 @@
+require 'test_helper'
+
+class RecipeTest < ActiveSupport::TestCase
+  def setup
+    @recipe = Recipe.new(name: "Vegetable", description: "great vegetable recipe")
+  end
+  
+  test "recipe should be valid" do
+    assert @recipe.valid?
+  end
+  
+  test "name should be present" do
+    @recipe.name = ""
+    assert_not @recipe.valid?
+  end
+  
+  test "description should be present" do
+    @recipe.description = ""
+    assert_not @recipe.valid?
+  end
+  
+  test "description shouldn´t be less than 5 characters" do
+    @recipe.description = "a"*3
+    assert_not @recipe.valid?
+  end
+  
+   test "description shouldn´t be more than 500 characters" do
+    @recipe.description = "a"*501
+    assert_not @recipe.valid?
+  end
+end
